3b3c35b1f9bd03f9e452ba266da6b8621aa309e2
Richard George <richard.george@faethm.ai>
2023-11-29 23:44:18 +0000

    minor stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from langchain.llms import OpenAI
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
+#from langchain.llms import OpenAI
+#from langchain.text_splitter import CharacterTextSplitter
+#from langchain.embeddings import OpenAIEmbeddings
+#from langchain.vectorstores import Chroma
+#from langchain.chains import RetrievalQA
 
 
 #st.set_page_config(page_title='🦜 Pearson Career Coach App')
