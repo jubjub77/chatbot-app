4d9356766e469d0e9b6010b357d7d5b5c7c85d48
Richard George <richard.george@faethm.ai>
2023-11-30 04:05:40 +0000

    new mess

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,155 +1,90 @@
-import streamlit as st
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains import ConversationalRetrievalChain
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.document_loaders.image import UnstructuredImageLoader
-from langchain.document_loaders import ImageCaptionLoader
-from langchain.docstore.document import Document
 import os
-#import pytube
-import openai
 
-# Chat UI title
-st.header("Upload your own files and ask questions like ChatGPT")
-st.subheader('File types supported: PDF/DOCX/TXT/JPG/PNG/YouTube :city_sunrise:')
+import streamlit as st
+from decouple import config
 
-# File uploader in the sidebar on the left
-with st.sidebar:
-    # Input for OpenAI API Key
-    openai_api_key = st.text_input("OpenAI API Key", type="password")
 
-    # Check if OpenAI API Key is provided
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import Chroma
+from langchain.chat_models import ChatOpenAI
+import chromadb
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
-    # Set OPENAI_API_KEY as an environment variable
-    os.environ["OPENAI_API_KEY"] = openai_api_key
 
-# Initialize ChatOpenAI model
-llm = ChatOpenAI(temperature=0, max_tokens=16000, model_name="gpt-3.5-turbo-16k", streaming=True)
+st.set_page_config(page_title="Chat With YT", layout="centered")
+st.title("Chat With YouTube Video")
 
-# Load version history from the text file
-def load_version_history():
-    with open("version_history.txt", "r") as file:
-        return file.read()
+# set API key
+os.environ["OPENAI_API_KEY"] = config("OPENAI_API_KEY")
 
-# Sidebar section for uploading files and providing a YouTube URL
-with st.sidebar:
-    uploaded_files = st.file_uploader("Please upload your files", accept_multiple_files=True, type=None)
-    youtube_url = st.text_input("YouTube URL")
+persistent_client = chromadb.PersistentClient(path="./vector_store_003")
 
-    # Create an expander for the version history in the sidebar
-    with st.sidebar.expander("**Version History**", expanded=False):
-        st.write(load_version_history())
+# embedding function
+embedding_function = SentenceTransformerEmbeddings(
+    model_name="all-MiniLM-L6-v2"
+)
 
-    st.info("Please refresh the browser if you decide to upload more files to reset the session", icon="🚨")
+vectordb = Chroma(
+    embedding_function=embedding_function,
+    persist_directory="./vector_store_003",
+    collection_name="david_gogginc_short"
+)
 
-# Check if files are uploaded or YouTube URL is provided
-if uploaded_files or youtube_url:
-    # Print the number of files uploaded or YouTube URL provided to the console
-    st.write(f"Number of files uploaded: {len(uploaded_files)}")
+prompt = PromptTemplate(
+    template="""Given the context of about a video. Answer the user in a friendly and precise manner.
+    
+    Context: {context}
+    
+    Human: {question}
+    
+    AI:""",
+    input_variables=["context", "question"]
+)
 
-    # Load the data and perform preprocessing only if it hasn't been loaded before
-    if "processed_data" not in st.session_state:
-        # Load the data from uploaded files
-        documents = []
+# set initial message
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Hello there, how can I help you"}
+    ]
 
-        if uploaded_files:
-            for uploaded_file in uploaded_files:
-                # Get the full file path of the uploaded file
-                file_path = os.path.join(os.getcwd(), uploaded_file.name)
+qa_chain = RetrievalQA.from_chain_type(
+    llm=ChatOpenAI(
+        temperature=0.6
+    ),
+    chain_type="stuff",
+    retriever=vectordb.as_retriever(),
+    return_source_documents=True,
+    chain_type_kwargs={
+        "prompt": prompt
+    }
+)
 
-                # Save the uploaded file to disk
-                with open(file_path, "wb") as f:
-                    f.write(uploaded_file.getvalue())
+# display messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-                # Check if the file is an image
-                if file_path.endswith((".png", ".jpg")):
-                    # Use ImageCaptionLoader to load the image file
-                    image_loader = ImageCaptionLoader(path_images=[file_path])
+# get user input
+user_prompt = st.chat_input()
 
-                    # Load image captions
-                    image_documents = image_loader.load()
+if user_prompt is not None:
+    st.session_state.messages.append({"role": "user", "content": user_prompt})
+    with st.chat_message("user"):
+        st.write(user_prompt)
 
-                    # Append the Langchain documents to the documents list
-                    documents.extend(image_documents)
-                    
-                elif file_path.endswith((".pdf", ".docx", ".txt")):
-                    # Use UnstructuredFileLoader to load the PDF/DOCX/TXT file
-                    loader = UnstructuredFileLoader(file_path)
-                    loaded_documents = loader.load()
 
-                    # Extend the main documents list with the loaded documents
-                    documents.extend(loaded_documents)
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Loading..."):
+            ai_response = qa_chain(
+                {"query": user_prompt}
+            )
+            st.write(ai_response["result"])
 
-        # Load the YouTube audio stream if URL is provided
-        if youtube_url:
-            youtube_video = pytube.YouTube(youtube_url)
-            streams = youtube_video.streams.filter(only_audio=True)
-            stream = streams.first()
-            stream.download(filename="youtube_audio.mp4")
-            # Set the API key for Whisper
-            openai.api_key = openai_api_key
-            with open("youtube_audio.mp4", "rb") as audio_file:
-                transcript = openai.Audio.transcribe("whisper-1", audio_file)
-            youtube_text = transcript['text']
+            for doc in ai_response["source_documents"]:
+                st.write(doc)
 
-            # Create a Langchain document instance for the transcribed text
-            youtube_document = Document(page_content=youtube_text, metadata={})
-            documents.append(youtube_document)
-
-        # Chunk the data, create embeddings, and save in vectorstore
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
-        document_chunks = text_splitter.split_documents(documents)
-
-        embeddings = OpenAIEmbeddings()
-        vectorstore = Chroma.from_documents(document_chunks, embeddings)
-
-        # Store the processed data in session state for reuse
-        st.session_state.processed_data = {
-            "document_chunks": document_chunks,
-            "vectorstore": vectorstore,
-        }
-
-    else:
-        # If the processed data is already available, retrieve it from session state
-        document_chunks = st.session_state.processed_data["document_chunks"]
-        vectorstore = st.session_state.processed_data["vectorstore"]
-
-    # Initialize Langchain's QA Chain with the vectorstore
-    qa = ConversationalRetrievalChain.from_llm(llm, vectorstore.as_retriever())
-
-    # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Accept user input
-    if prompt := st.chat_input("Ask your questions?"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Query the assistant using the latest chat history
-        result = qa({"question": prompt, "chat_history": [(message["role"], message["content"]) for message in st.session_state.messages]})
-
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            full_response = ""
-            full_response = result["answer"]
-            message_placeholder.markdown(full_response + "|")
-        message_placeholder.markdown(full_response)    
-        print(full_response)
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
-
-else:
-    st.write("Please upload your files and provide a YouTube URL for transcription.")
+    new_ai_message = {"role": "user", "content": ai_response}
+    st.session_state.messages.append(new_ai_message)
