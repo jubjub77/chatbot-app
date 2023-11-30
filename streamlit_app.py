b72d2b9b8d8372a46563407e0d8ee30dc0cb5bfd
Richard George <richard.george@faethm.ai>
2023-11-30 02:31:46 +0000

    basic set up to test

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,26 @@ from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 
 
-st.set_page_config(page_title='🦜 Pearson Career Coach App')
+def generate_response(uploaded_file, openai_api_key, query_text):
+    # Load document if file is uploaded
+    if uploaded_file is not None:
+        documents = [uploaded_file.read().decode()]
+    # Split documents into chunks
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.create_documents(documents)
+    # Select embeddings
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    # Create a vectorstore from documents
+    db = Chroma.from_documents(texts, embeddings)
+    # Create retriever interface
+    retriever = db.as_retriever()
+    # Create QA chain
+    qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+    return qa.run(query_text)
+
+
+
+st.set_page_config(page_title='🦜 Pearsonify')
 st.title('🦜 Welcome to Pearsonify, your personal career coach')
 
 
@@ -15,6 +34,22 @@ st.title('🦜 Welcome to Pearsonify, your personal career coach')
 uploaded_file = st.file_uploader('Upload an your resume, it can be in any format such as a downloaded linkedin pdf', type='txt')
 
 ## query
-query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
+query_text = st.text_input('Query:', placeholder = 'Enter your query here', disabled=not uploaded_file)
 
-st.write('Hello world!')
+#st.write('Hello world!')
+
+
+# Form input and query
+result = []
+with st.form('myform', clear_on_submit=True):
+    openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
+    if submitted and openai_api_key.startswith('sk-'):
+        with st.spinner('Calculating...'):
+            response = generate_response(uploaded_file, openai_api_key, query_text)
+            result.append(response)
+            del openai_api_key
+
+if len(result):
+    st.info(response)
+
