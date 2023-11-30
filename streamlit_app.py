113e3eccd4f555bcbf7bd2c046ca71b4a3e01861
Richard George <richard.george@faethm.ai>
2023-11-30 04:32:49 +0000

    going back

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,54 @@
 import streamlit as st
-from streamlit_chat import message
-from utils import get_initial_message, get_chatgpt_response, update_chat
-import os
-from dotenv import load_dotenv
-load_dotenv()
-import openai
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
+from langchain.llms import openai
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
 
-st.title("Chatbot : ChatGPT and Streamlit Chat")
-st.subheader("AI Tutor:")
 
-model = st.selectbox(
-    "Select a model",
-    ("gpt-3.5-turbo", "gpt-4")
-)
+def generate_response(uploaded_file, openai_api_key, query_text):
+    # Load document if file is uploaded
+    if uploaded_file is not None:
+        documents = [uploaded_file.read().decode()]
+    # Split documents into chunks
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.create_documents(documents)
+    # Select embeddings
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    # Create a vectorstore from documents
+    db = Chroma.from_documents(texts, embeddings)
+    # Create retriever interface
+    retriever = db.as_retriever()
+    # Create QA chain
+    qa = RetrievalQA.from_chain_type(llm=openai(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+    return qa.run(query_text)
 
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
 
-query = st.text_input("Query: ", key="input")
 
-if 'messages' not in st.session_state:
-    st.session_state['messages'] = get_initial_message()
- 
-if query:
-    with st.spinner("generating..."):
-        messages = st.session_state['messages']
-        messages = update_chat(messages, "user", query)
-        # st.write("Before  making the API call")
-        # st.write(messages)
-        response = get_chatgpt_response(messages,model)
-        messages = update_chat(messages, "assistant", response)
-        st.session_state.past.append(query)
-        st.session_state.generated.append(response)
-        
-if st.session_state['generated']:
+st.set_page_config(page_title='🦜 Pearsonify')
+st.title('🦜 Welcome to Pearsonify, your personal career coach')
 
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-        message(st.session_state["generated"][i], key=str(i))
 
-    with st.expander("Show Messages"):
-        st.write(messages)
+## a good place to start is with a CV
+uploaded_file = st.file_uploader('To help your career coach get a quick snapshot of your career history, why not upload a resume? Load a pdf version of your linkedin profile or resume ', type='pdf')
+
+## query
+query_text = st.text_input('Let\'s talk:', placeholder = 'Enter your thoughts here')
+
+#st.write('Hello world!')
+
+# Form input and query
+result = []
+with st.form('myform', clear_on_submit=True):
+    openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
+    if submitted and openai_api_key.startswith('sk-'):
+        with st.spinner('Calculating...'):
+            response = generate_response(uploaded_file, openai_api_key, query_text)
+            result.append(response)
+            del openai_api_key
+
+if len(result):
+    st.info(response)
+
